cc9d3182ff31005e6ce59b5d2292c3660fb858a4
AsatoSeto <leshamoklyakov@gmail.com>
2018-08-07 09:11:03 +0300

    test
    Взял тебя за яйки

--- Models.h
+++ Models.h
@@ -4,7 +4,7 @@ class Models
 	public:
 	void drawCube(float xrf,float yrf,float zrf, float stepx, float stepy, float stepz)
 	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glLoadIdentity();
 		glTranslatef(stepx, stepy, stepz);
@@ -14,7 +14,57 @@ class Models
 		glRotatef(zrf, 0.0f, 0.0f, 1.0f);
 
 		glBegin(GL_TRIANGLES);
-		
+		glColor3f(1.0f, 0.0f, 1.0f);
+		glVertex3f(1.000000,-1.000000,1.000000);
+		glVertex3f(-1.000000,-1.000000,-1.000000);
+		glVertex3f(1.000000,-1.000000,-1.000000);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+		glVertex3f(0.999999,1.000000,1.000001);
+		glVertex3f(1.000000,1.000000,-0.999999);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(1.000000,1.000000,-0.999999);
+		glVertex3f(1.000000,-1.000000,1.000000);
+		glVertex3f(1.000000,-1.000000,-1.000000);
+		glColor3f(1.0f, 0.0f, 1.0f);
+		glVertex3f(0.999999,1.000000,1.000001);
+		glVertex3f(-1.000000,-1.000000,1.000000);
+		glVertex3f(1.000000,-1.000000,1.000000);
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(-1.000000,-1.000000,1.000000);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+		glVertex3f(-1.000000,-1.000000,-1.000000);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(1.000000,-1.000000,-1.000000);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+		glVertex3f(1.000000,1.000000,-0.999999);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(1.000000,-1.000000,1.000000);
+		glVertex3f(-1.000000,-1.000000,1.000000);
+		glVertex3f(-1.000000,-1.000000,-1.000000);
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+		glVertex3f(-1.000000,1.000000,1.000000);
+		glVertex3f(0.999999,1.000000,1.000001);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex3f(1.000000,1.000000,-0.999999);
+		glVertex3f(0.999999,1.000000,1.000001);
+		glVertex3f(1.000000,-1.000000,1.000000);
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(0.999999,1.000000,1.000001);
+		glVertex3f(-1.000000,1.000000,1.000000);
+		glVertex3f(-1.000000,-1.000000,1.000000);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(-1.000000,-1.000000,1.000000);
+		glVertex3f(-1.000000,1.000000,1.000000);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(1.000000,-1.000000,-1.000000);
+		glVertex3f(-1.000000,-1.000000,-1.000000);
+		glVertex3f(-1.000000,1.000000,-1.000000);
+
 		glEnd();
+	
+	
 	}
 };
